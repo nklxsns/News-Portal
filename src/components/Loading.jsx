6824939aeb6b3b6c6aa73c041fdb93e1d1754d7f
nklxsns <nikhilsuryakant4@gmail.com>
2024-06-20 23:51:13 +0530

    loading animation added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div class="text-center my-4">
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
